b79a0b2e4682c2a1807d2a1b81e6d87d7ccda8af
Artem Babii <Artem.Babii@infopulse.com>
2024-12-12 11:41:45 -0400

    Remove unnecessary stuff

---
+++ FootballLeague/wwwroot/js/nav-buttons.js
@@ -0,0 +1,28 @@
+﻿import { LitElement, html, css } from 'lit-element';
+
+export class MyComponent extends LitElement {
+    firstUpdated() {
+        this.count = 0;
+        const shadow = this.shadowRoot;
+        const buttonLeague1 = shadow.querySelector("button");
+        buttonLeague1.onclick = (e) => {
+            this.count++;
+            this.dispatchEvent(new CustomEvent("clicked", {
+                detail: this.count //data
+            }));
+        }
+
+        const buttonLeague2 = shadow.querySelector("button");
+        buttonLeague2.onclick = (e) => {
+            this.count++;
+            this.dispatchEvent(new CustomEvent("clicked", {
+                detail: this.count //data
+            }));
+        }
+    }
+
+    render() {
+        return html`<button>Click me!</button>
+    <child-component> </child-component>`;
+    }
+}
