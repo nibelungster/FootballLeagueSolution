4356a68e2e4501a46173aefef4bd99d0a219ea78
Artem Babii <Artem.Babii@infopulse.com>
2024-12-12 11:41:45 -0400

    Revrite custom component

--- FootballLeague/wwwroot/js/match-result.js
+++ FootballLeague/wwwroot/js/match-result.js
@@ -3,32 +3,6 @@
         super()
 
         const shadow = this.attachShadow({ mode: "open" });
-
-        const container = this.container = document.createElement("div");
-        container.setAttribute("class", "container");
-
-        const team1 = this.container.team1 = document.createElement("div");
-        team1.setAttribute("class", "inside");
-
-        const team2 = this.container.team2 = document.createElement("div");
-        team2.setAttribute("class", "inside");
-
-        const icon = this.container.icon = document.createElement("div");
-        icon.setAttribute("class", "icon");
-        icon.setAttribute("tabindex", 0);
-
-        let imgUrl;
-        if (this.hasAttribute("img")) {
-            imgUrl = this.getAttribute("img");
-        } else {
-            imgUrl = "favicon.ico";
-        }
-
-        const img = this.container.icon.img = document.createElement("img");
-        img.src = imgUrl;
-        icon.appendChild(img);
-
-        // Create some CSS to apply to the shadow dom
         const style = document.createElement("style");
         console.log(style.isConnected);
 
@@ -69,20 +43,68 @@
 
         shadow.appendChild(style);
         console.log(style.isConnected);
-        shadow.appendChild(container);
-        container.appendChild(team1);
-        container.appendChild(team2);
-        container.appendChild(icon);
     }
 
     connectedCallback() {
-        // Create a shadow root
-        const team1 = this.getAttribute("team1");
-        const team2 = this.getAttribute("team2");
+        const homeTeamScore = this.getAttribute("homeTeamScore");
+        const awayTeamScore = this.getAttribute("awayTeamScore");
+        const homeTeamImage = this.getAttribute("homeTeamImage");
+        const awayTeamImage = this.getAttribute("awayTeamImage");
         const imgSrc = this.getAttribute("imgSrc");
-        this.container.team1.innerText = team1;
-        this.container.team2.innerText = team2;
-        this.container.icon.img.src = imgSrc;
+
+        this.shadowRoot.innerHTML = `
+        <style>
+
+    .leagueimg {
+        width:40px;
+        height:40px;
+    }
+
+    .container {
+        border: 1px solid black;
+        overflow-y: hidden;
+        white-space: nowrap;
+        background: yellow;
+        border-radius: 10px;
+        padding-top: 5px;
+        padding-right: 5px;
+        padding-bottom: 5px;
+        padding-left: 5px;
+    }
+
+    .inside {
+        display: inline-block;
+        background: white;
+        border-radius: 2px;
+        width: 40%;
+        text-align: center;
+        border: 1px green;
+        padding-top: 1px;
+        padding-right: 1px;
+        padding-bottom: 1px;
+        padding-left: 1px;
+    }
+
+    .icon:hover + .info, .icon:focus + .info {
+        opacity: 1;
+    }
+
+
+        </style>
+        <div class="container" >
+            <div>
+                <img src="${imgSrc}" class="leagueimg">
+            </div>
+            <div class="inside">
+                <p>${homeTeamScore}</p>
+                <img src="${homeTeamImage}" style="width:40px">
+            </div>
+            <div class="inside">
+                <p>${awayTeamScore}</p>
+                <img src="${awayTeamImage}" style="width:40px">
+            </div>
+        </div>
+        `;
     }
 }
 
